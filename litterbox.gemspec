79202271740ef7a365e66b832f494fb9348dfb01
skylerto <skylerclayne@gmail.com>
2018-02-27 19:00:01 -0500

    download and clean up
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- litterbox.gemspec
+++ litterbox.gemspec
@@ -29,4 +29,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop', '~> 0.52.1'
+  spec.add_development_dependency 'rubyzip', '~> 1.2.1'
 end
--- spec/litterbox_spec.rb
+++ spec/litterbox_spec.rb
@@ -17,4 +17,9 @@ RSpec.describe Litterbox do
     expect(last_build.pkg_sha256sum).to eql '0fc503f9ebf5bde670176dd3eea6357a0ec9beb9738320cec8a4cdaa145f410b'
     expect(last_build.pkg_blake2bsum).to eql 'ec6909fa3eb5f80d50de2002a3baf038b75f9dfde6147974874a7f46336885b5'
   end
+
+  it 'builds a habitat package' do
+    download_hab_plan
+    cleanup_hab_plan
+  end
 end
