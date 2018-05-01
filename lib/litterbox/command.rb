b242e914bce687af6c3a1b366ecaf59af0666c6d
skylerto <skylerclayne@gmail.com>
2018-05-01 10:35:06 -0400

    get rid of it
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- lib/litterbox/command.rb
+++ lib/litterbox/command.rb
@@ -3,15 +3,18 @@ module Litterbox
   module Command
     class << self
       def exec(cmd)
+        out, err = ''
         cmd = "sudo #{cmd}" if ENV['USE_SUDO']
         Open3.popen3(cmd) do |_, stdout, stderr, thr|
           while (line = stderr.gets)
+            err << line
             puts(line)
           end
           while (line = stdout.gets)
+            out << line
             puts(line)
           end
-          return thr
+          return out, err, thr
         end
       end
     end
