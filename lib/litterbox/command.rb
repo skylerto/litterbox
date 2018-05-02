f3ce097cc5f9dc77412624c5209cee97dd78ac6c
skylerto <skylerclayne@gmail.com>
2018-05-02 11:07:25 -0400

    conditional logging
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- lib/litterbox/command.rb
+++ lib/litterbox/command.rb
@@ -2,22 +2,23 @@ module Litterbox
   # CLI Command operations
   class Command
     attr_accessor :stdout, :stderr, :process
-    def initialize(cmd)
+    def initialize(cmd, log = ENV['LITTERBOX_LOG'])
       @cmd = cmd
       @cmd = "sudo #{@cmd}" if ENV['USE_SUDO']
       @stdout = ''
       @stderr = ''
+      @log = log
     end
 
     def run_command
       Open3.popen3(@cmd) do |_, out, err, thr|
         while (line = err.gets)
           @stderr << line unless line.nil?
-          puts(line)
+          puts(line) if @log
         end
         while (lines = out.gets)
           @stdout << lines unless lines.nil?
-          puts(lines)
+          puts(lines) if @log
         end
         @process = thr.value
         return @stdout, @stderr, @process
